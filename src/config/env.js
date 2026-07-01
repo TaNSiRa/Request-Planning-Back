1eb14abc5581c9d7ce7e9e9afaaa1abd477ba3c5
TaNSiRa <tansirawit2543@gmail.com>
2026-07-01 15:20:08 +0700

    .

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,8 @@
-﻿require("dotenv").config();
+﻿const path = require("path");
+
+require("dotenv").config({
+  path: path.resolve(__dirname, "../../.env"),
+});
 
 function boolEnv(name, defaultValue = false) {
   const value = process.env[name];
